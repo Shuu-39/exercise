e06e08d3d1305ac205bb8909165030923833bd2d
Shuu-39 <1397402891@qq.com>
2026-02-05 02:43:10 +0800

    new file:   flight.lua

---
+++ flight.lua
@@ -0,0 +1,6 @@
+while (true) do
+    redstone.setOutput("back", 15)
+    sleep(0.5)
+    redstone.setOutput("back", 0)
+    sleep(0.5)
+end
---
+++ pythonexercise1.py
@@ -0,0 +1,2 @@
+print(reduce (lambda x, y: x if x > y else y), [3, 6, 2, 8, 4])
+"""this is a test"""
